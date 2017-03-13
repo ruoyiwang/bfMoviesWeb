90a1335520fad177de2ad6b1951aad0b546fc5fd
ruoyiwang <wang.ruoyi.uw@gmail.com>
2017-03-12 23:35:57 -0400

    refactoring factory into service

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,38 +1,12 @@
-app.controller('MainController', ['$scope', 'getMovies', function($scope, getMovies) {
-    $scope.title = 'This is a title';
-    $scope.uid = "this is uid";
+app.controller('MainController', ['$scope', 'movieService', function($scope, movieService) {
 
-    getMovies.success(function (data) {
-        $scope.movies = data;
+    // gets the default movies
+    movieService.getMovies(function(data) {
+        var result = data.data;
+        console.log(result);
+        $scope.movies = result;
 
+    }, function(err) {
+        console.log(err);
     });
-    //
-    // $scope.movies = [
-    //     {
-    //         poster: 'img/move.jpg',
-    //         title: 'MOVE',
-    //         genre: 'MOVE, Inc.',
-    //         actors: "temp actor 1"
-    //     },
-    //     {
-    //         poster: 'img/shutterbugg.jpg',
-    //         title: 'Shutterbugg',
-    //         genre: 'Chico Dusty',
-    //         actors: "temp actor 2"
-    //     },
-    //     {
-    //         poster: 'img/gameboard.jpg',
-    //         title: 'Gameboard',
-    //         genre: 'Armando Perez',
-    //         actors: "temp actor 3"
-    //     },
-    //     {
-    //         poster: 'img/forecast.jpg',
-    //         title: 'Forecast',
-    //         genre: 'Forecast',
-    //         actors: "temp actor 4"
-    //     }
-    // ];
 }]);
-
-
--- js/services/getMovies.js
+++ js/services/getMovies.js
@@ -1,14 +1,19 @@
-app.factory('getMovies', ['$http', function($http) {
+app.service('movieService', ['$http', function($http) {
 
-    return $http({
+    this.getMovies = function(callback, error) {
+        return $http({
             method: 'GET',
-            url: 'https://6lk6s51xv6.execute-api.us-east-1.amazonaws.com/prod/GetMoviesForUser?uid=1'
-        })
-        .success(function(data) {
-            console.log(data);
-            return data;
-        })
-        .error(function(err) {
-            return err;
-        });
+            url: 'https://6lk6s51xv6.execute-api.us-east-1.amazonaws.com/prod/GetMoviesForUser'
+        }).then(callback, error);
+    };
+
+
+    this.getMoviesForUser = function(uid, callback, error) {
+        return $http({
+            method: 'GET',
+            url: 'https://6lk6s51xv6.execute-api.us-east-1.amazonaws.com/prod/GetMoviesForUser?uid='+uid
+        }).then(callback, error);
+    };
+
+
 }]);
